2986a227da2404417ce03125ff21f961fcb17e33
Emmanuele Villa <emmanuele.villa@finconsgroup.com>
2022-11-22 17:04:20 +0100

    first commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class GameManager : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Bokluk-s-dragon-heist

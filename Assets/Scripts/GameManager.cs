59358f2c3a34e8b534af9c215bb4d9ad9d6f7d59
Emmanuele Villa <emmanuele.villa@finconsgroup.com>
2022-11-22 17:37:26 +0100

    init cave

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,17 +1,6 @@
 using UnityEngine;
 
-namespace Assets.Scripts
-{
-    public class GameManager : MonoBehaviour
-    {
-        // Start is called before the first frame update
-        private void Start()
-        {
-        }
-
-        // Update is called once per frame
-        private void Update()
-        {
-        }
+namespace Assets.Scripts {
+    public class GameManager : MonoBehaviour {
     }
 }
